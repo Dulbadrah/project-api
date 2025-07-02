da42c4cdb4042b724ca28ff50d27123161519330
25LP5306 25LP5306 <25LP5306@pinecones-iMac.local>
2025-07-02 12:39:59 +0800

    mmgg

--- src/constroller/food/get-food.ts
+++ src/constroller/food/get-food.ts
@@ -3,9 +3,38 @@ import FoodModel from "../../Model/food";
 
 export const getFood = async (req: Request, res: Response) => {
   try {
-    const food = await FoodModel.find();
-    res.status(200).send({succes: true, food});
+    const food = await FoodModel.aggregate([
+      {
+        $lookup: {
+          from: "foodcategories",
+          localField: "category",
+          foreignField: "_id",
+          as: "foodCategory",
+        },
+      },
+      {
+        $unwind: "$foodCategory",
+      },
+      {
+        $group: {
+          _id: "$foodCategory._id",
+          categoryName: { $first: "$foodCategory.categoryName" },
+          foods: {
+            $push: {
+              _id: "$_id",
+              foodName: "$foodName",
+              price: "$price",
+              image: "$image",
+              ingredients: "$ingredients",
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    res.status(200).json({ success: true, food });
   } catch (error) {
-    res.status(404).send({ message: "API error", error });
+    res.status(500).json({ message: "API error", error });
   }
 };
